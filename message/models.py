fe92cf51ff507e66ad12478126f6f0d4449ee05f
Ben Scholom <bscholom@gmail.com>
2017-08-14 17:20:21 +0300

    basic admin

--- message/models.py
+++ message/models.py
@@ -2,11 +2,13 @@ from __future__ import unicode_literals
 from django.db import models
 from django.utils import timezone
 import datetime
+
+
 # Create your models here.
-class post:
-    post_title = models.CharField(max_length = 50)
-    post_author = models.CharField(max_length = 20)
-    post_text = models.TextField
+class Post(models.Model):
+    post_title = models.CharField(max_length=50)
+    post_author = models.CharField(max_length=20)
+    post_text = models.TextField()
     post_date = models.DateTimeField('date published')
     post_creation = models.DateTimeField(default=timezone.now())
 
@@ -14,8 +16,7 @@ class post:
         return self.post_title
 
     def published_date(self):
-        return self.post_date >= timezone.now() - datetime.timedelta(days = 1)
+        return self.post_date >= timezone.now() - datetime.timedelta(days=1)
 
 
-    #remeber modelform
-
+        # remeber modelform
