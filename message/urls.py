32418990995082e87bcd31b463faa342e10f8843
Ben Scholom <bscholom@gmail.com>
2017-08-15 15:52:26 +0300

    added page for specific posts

--- message/urls.py
+++ message/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.posts, name='posts'),
+    url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
 ]
--- message/views.py
+++ message/views.py
@@ -6,4 +6,8 @@ from django.utils import timezone
 #function to submit posts to template in order
 def posts(request):
     plist = Post.objects.filter(post_date__lte=timezone.now()).order_by('-post_date')
-    return render(request, 'message/index.html', {'posts': plist})
+    return render(request, 'message/post.html', {'posts': plist})
+#puts only one post on a page dedicated to that post
+def post_detail(request, pk):
+    selectedPost = Post.objects.get(pk=pk)
+    return render(request, 'message/post_detail.html', {'post': selectedPost})
